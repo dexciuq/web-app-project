23a5dd35046885a1f748ae128a1f787b8b4d55a4
Arman <skaifai.99@mail.ru>
2023-01-11 00:01:56 +0600

    Added post and comment model

---
+++ quitter/src/main/java/com/company/quitter/model/Comment.java
@@ -0,0 +1,39 @@
+package com.company.quitter.model;
+
+import com.company.quitter.Main;
+import com.company.quitter.model.enumiration.UserRole;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Document(collection = "comments")
+public class Comment {
+    @Id
+    private String id;
+    private List<Comment> comments;
+    @DBRef
+    private String userID;
+    private String text;
+    private int likeCount;
+    private String creationDate;
+
+    public Comment(String id,
+                   List<Comment> comments,
+                   String userID,
+                   String text,
+                   int likeCount) {
+        this.id = id;
+        this.comments = comments;
+        this.userID = userID;
+        this.text = text;
+        this.likeCount = likeCount;
+        this.creationDate = LocalDateTime.now().format(Main.dataFormatter);
+    }
+}
--- quitter/src/main/java/com/company/quitter/model/Post.java
+++ quitter/src/main/java/com/company/quitter/model/Post.java
@@ -1,5 +1,43 @@
 package com.company.quitter.model;
 
-public class Post {
+import com.company.quitter.Main;
+import com.company.quitter.model.enumiration.UserRole;
+import lombok.Data;
+import org.springframework.cglib.core.Local;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Document(collection = "posts")
+public class Post {
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private String imageURL;
+    private List<Comment> comments;
+    private List<String> tags;
+    private int likeCount;
+    private String creationDate;
+
+    public Post(String id,
+                String title,
+                String description,
+                String imageURL,
+                List<Comment> comments,
+                List<String> tags,
+                int likeCount) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.imageURL = imageURL;
+        this.comments = comments;
+        this.tags = tags;
+        this.likeCount = likeCount;
+        this.creationDate = LocalDateTime.now().format(Main.dataFormatter);
+    }
 }
